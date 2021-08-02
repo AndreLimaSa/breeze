a330ead61b35a7fa00aaf8105a43b95745314730
AndreLimaSa <88165971+AndreLimaSa@users.noreply.github.com>
2021-08-02 23:12:17 +0100

    Update final.js

--- final.js
+++ final.js
@@ -347,7 +347,7 @@ function check(input)
 }
 
 //function slideshow
-var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6",
 "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13",
 "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18", "mySlides19", "mySlides20",
@@ -567,8 +567,8 @@ btnGet.addEventListener('click', () => {
 
 if ((checkpontedelima.checked == true) && (checkpraia.checked == true)){
     newArray9.forEach(emp9 => {
+      document.getElementById("praiafluvialdavaleta").style.display = "block";
       document.getElementById("praiafluvialdaazenhadeestoraos").style.display = "block";
-      document.getElementById("praiapedonegro").style.display = "block";
     });
   }
 
@@ -1015,7 +1015,6 @@ if (checkpontedelima.checked == true){
           document.getElementById("praiafluvialdaazenhadeestoraos").style.display = "block";
           document.getElementById("percursodasveigas").style.display = "block";
           document.getElementById("percursodastapadas").style.display = "block";
-          document.getElementById("merendasboamorte").style.display = "block";
           document.getElementById("praiapedonegro").style.display = "block";
     });
   }
@@ -1660,3 +1659,4 @@ function fullsize(){
     document.getElementById("slideshow-container").style.width = "300px";
 
 }
+
